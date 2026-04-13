3dc744a48889154db786db024a7551915384a719
sergiocalazans <calazanssergiohenrique@gmail.com>
2026-04-13 12:48:32 -0300

    Update interfaces

--- telaJogar.js
+++ telaJogar.js
@@ -1,12 +1,11 @@
 function telaJogar() {
   desenharMoldura();
-  
-  // Título da Tela [cite: 5]
+
   fill(255);
   noStroke();
   textSize(28);
   text("JOGAR", width / 2, 100);
-  
+
   textSize(12);
   fill(200);
   text("PRESSIONE ESC PARA VOLTAR", width / 2, height - 60);
--- telaSobre.js
+++ telaSobre.js
@@ -1,17 +1,18 @@
 function telaSobre() {
   desenharMoldura();
 
-  // Título da Tela [cite: 5]
   fill(255);
   noStroke();
   textSize(28);
   text("SOBRE", width / 2, 100);
 
-  // Conteúdo informativo
   textSize(16);
-  text("Guitar Letter é um jogo rítmico\n educativo para praticar digitação.", width / 2, height / 2);
+  text(
+    "Guitar Letter é um jogo rítmico\n educativo para praticar digitação.",
+    width / 2,
+    height / 2,
+  );
 
-  // Instrução de retorno [cite: 6]
   textSize(12);
   fill(200);
   text("PRESSIONE ESC PARA VOLTAR", width / 2, height - 60);
